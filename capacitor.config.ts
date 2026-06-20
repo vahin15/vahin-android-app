0b8805724ee6c7d6376c093ee1f96475d880e586
vahin15 <swati.saxena06@gmail.com>
2026-06-20 18:18:27 +0530

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.vahin.connect',
+  appName: 'Vahin Connect',
+  webDir: 'www'
+};
+
+export default config;
